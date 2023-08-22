a3626840618e35dc6d9446d2a3c139532fbc2c1b
Antoine Kahlouche <antoine@kahlouche.fr>
2023-08-22 07:38:36 -0400

    update

--- nvim/lua/antoinekahlouche/packer.lua
+++ nvim/lua/antoinekahlouche/packer.lua
@@ -6,9 +6,9 @@ return require('packer').startup(function(use)
 
     use 'navarasu/onedark.nvim'
 
-    use {'nvim-tree/nvim-web-devicons'}
+    use 'nvim-tree/nvim-web-devicons' 
 
-    use {'nvim-tree/nvim-tree.lua'}
+    use 'nvim-tree/nvim-tree.lua' 
 
     use {
         'nvim-lualine/lualine.nvim',
@@ -17,25 +17,26 @@ return require('packer').startup(function(use)
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.2',
-        requires = { {'nvim-lua/plenary.nvim'} }
+        requires = { { 'nvim-lua/plenary.nvim' } }
     }
     use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
-    use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use {'nvim-treesitter/nvim-treesitter',  run = ':TSUpdate' }
 
     use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
         requires = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {'williamboman/mason.nvim'},           -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},     -- Required
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {'L3MON4D3/LuaSnip'},     -- Required
+            { 'hrsh7th/nvim-cmp' },                  -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },              -- Required
+            { 'L3MON4D3/LuaSnip' },                  -- Required
         }
     }
 
+    use "Pocco81/auto-save.nvim"
 end)
