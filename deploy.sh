88c4ae7145dc9097162e241c2b46a56d98cdd5b2
Antoine Kahlouche <antoine@kahlouche.fr>
2023-11-16 22:17:27 -0500

    tmux conf

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# alacritty
+cp -f alacritty.yml ~/.config/alacritty/alacritty.yml
+
 # nvim
 rm -rf ~/.config/nvim
 cp -Rf ./nvim/ ~/.config/nvim/
 
+# tmux
+cp -f .tmux.conf ~/.tmux.conf
+
 # zsh
 cp -f .zshrc ~/.zshrc
-
-# alacritty
-cp -f alacritty.yml ~/.config/alacritty/alacritty.yml
