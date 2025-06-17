80cae55d8240262bf557b64a901ed240915441c6
Antoine Kahlouche <antoine@kahlouche.fr>
2025-06-17 15:34:35 +1000

    update config

--- nvim/lua/antoinekahlouche/plugins/conform.lua
+++ nvim/lua/antoinekahlouche/plugins/conform.lua
@@ -1,30 +1,31 @@
 return {
-    'stevearc/conform.nvim',
+	"stevearc/conform.nvim",
 
-    config = function()
-        local conform = require("conform")
-            
-        conform.setup({
-            formatters_by_ft = {
-                javascript = { "prettier", stop_after_first = true },
-                javascriptreact = { "prettier", stop_after_first = true },
-                typescriptreact = { "prettier", stop_after_first = true },
-                astro = { "prettier", stop_after_first = true },
-                css = { "prettier", stop_after_first = true },
-                html = { "prettier", stop_after_first = true },
-                json = { "prettier", stop_after_first = true },
-                lua = { "stylua", stop_after_first = true },
-                markdown = { "prettier", stop_after_first = true },
-                typescript = { "prettier", stop_after_first = true },
-                yaml = { "prettier", stop_after_first = true },
-            },
-        })
+	config = function()
+		local conform = require("conform")
 
-        vim.api.nvim_create_autocmd("InsertLeave", {
-            pattern = "*",
-            callback = function(args)
-                conform.format({ bufnr = args.buf })
-            end,
-        })
-    end
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettier", stop_after_first = true },
+				javascriptreact = { "prettier", stop_after_first = true },
+				typescriptreact = { "prettier", stop_after_first = true },
+				astro = { "prettier", stop_after_first = true },
+				css = { "prettier", stop_after_first = true },
+				html = { "prettier", stop_after_first = true },
+				jsonc = { "prettier", stop_after_first = true },
+				json = { "prettier", stop_after_first = true },
+				lua = { "stylua", stop_after_first = true },
+				markdown = { "prettier", stop_after_first = true },
+				typescript = { "prettier", stop_after_first = true },
+				yaml = { "prettier", stop_after_first = true },
+			},
+		})
+
+		vim.api.nvim_create_autocmd("InsertLeave", {
+			pattern = "*",
+			callback = function(args)
+				conform.format({ bufnr = args.buf })
+			end,
+		})
+	end,
 }
--- nvim/lua/antoinekahlouche/plugins/lazy-git.lua
+++ nvim/lua/antoinekahlouche/plugins/lazy-git.lua
@@ -1,3 +1,5 @@
+vim.g.lazygit_floating_window_scaling_factor = 1
+
 return {
 	"kdheepak/lazygit.nvim",
 	lazy = true,
@@ -8,12 +10,6 @@ return {
 		"LazyGitFilter",
 		"LazyGitFilterCurrentFile",
 	},
-	-- optional for floating window border decoration
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	-- setting the keybinding for LazyGit with 'keys' is recommended in
-	-- order to load the plugin when the command is run for the first time
 	keys = {
 		{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
 	},
