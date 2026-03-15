f08e3e51d5b73454b1a279e4501621530d2de5dd
Antoine Kahlouche <antoine@kahlouche.fr>
2026-03-15 23:09:25 +0100

    🔧 CHORE: Refine buffer close and tree highlights

--- nvim/lua/default/plugins/bufferline.lua
+++ nvim/lua/default/plugins/bufferline.lua
@@ -41,7 +41,7 @@ return {
 		map("n", "<A-9>", "<Cmd>BufferLineGoToBuffer 9<CR>", opts)
 		map("n", "<A-0>", "<Cmd>BufferLineGoToBuffer 10<CR>", opts)
 
-		map("n", "q", "<Cmd>Bdelete<CR>", opts)
+		map("n", "q", "<Cmd>!Bdelete<CR>", opts)
 		-- map("n", "qo", "<Cmd>BufferLineCloseOthers<CR>", opts)
 	end,
 }
