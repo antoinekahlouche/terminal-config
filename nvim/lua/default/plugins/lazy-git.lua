ba6cf7b5911f0d861075d10b1a6b3ed2a3622a8b
Antoine Kahlouche <antoine@kahlouche.fr>
2026-03-16 14:45:36 +0100

    fix(nvim): close lazygit cleanly on escape

--- nvim/lua/default/plugins/lazy-git.lua
+++ nvim/lua/default/plugins/lazy-git.lua
@@ -13,4 +13,18 @@ return {
 	init = function()
 		vim.cmd([[cnoreabbrev <expr> git getcmdtype() == ':' && getcmdline() ==# 'git' ? 'LazyGit' : 'git']])
 	end,
+	config = function()
+		vim.api.nvim_create_autocmd("TermOpen", {
+			pattern = "term://*lazygit",
+			callback = function(args)
+				vim.keymap.set("t", "<Esc>", function()
+					local job = vim.b[args.buf].terminal_job_id
+
+					if job ~= nil then
+						vim.api.nvim_chan_send(job, "q")
+					end
+				end, { buffer = args.buf, nowait = true, silent = true })
+			end,
+		})
+	end,
 }
