07ac4eb225b40d0d8f5e0a7e38db5e320fcee750
Antoine Kahlouche <antoine@kahlouche.fr>
2026-04-01 17:14:13 +0200

    fix(nvim): update blink completion keybindings

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		"saghen/blink.cmp",
+		optional = true,
+		opts = {
+			keymap = {
+				preset = "none",
+				["<Tab>"] = { "select_next", "fallback" },
+				["<S-Tab>"] = { "select_prev", "fallback" },
+				["<CR>"] = { "accept", "fallback" },
+			},
+			completion = {
+				list = {
+					selection = {
+						preselect = false,
+					},
+				},
+			},
+		},
+	},
+}
