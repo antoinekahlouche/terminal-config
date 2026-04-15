029cc75c7b454ec0f0cb0eb18aac6eb8ba35a481
Antoine Kahlouche <antoine@kahlouche.fr>
2026-04-15 15:52:10 +0200

    refactor(nvim): simplify conform formatter registration

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,20 +1,26 @@
 return {
 	"stevearc/conform.nvim",
 	opts = function(_, opts)
+		local formatter_languages = {
+			oxfmt = {
+				"javascript",
+				"javascriptreact",
+				"typescript",
+				"typescriptreact",
+				"json",
+				"jsonc",
+				"css",
+			},
+		}
+
 		opts.format_on_save = false
 
 		opts.formatters_by_ft = opts.formatters_by_ft or {}
 
-		for _, ft in ipairs({
-			"javascript",
-			"javascriptreact",
-			"typescript",
-			"typescriptreact",
-			"json",
-			"jsonc",
-			"css",
-		}) do
-			opts.formatters_by_ft[ft] = { "oxfmt" }
+		for formatter, languages in pairs(formatter_languages) do
+			for _, ft in ipairs(languages) do
+				opts.formatters_by_ft[ft] = { formatter }
+			end
 		end
 	end,
 }
