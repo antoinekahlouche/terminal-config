031bcd7f97a929619cfa0f186770d965358ff017
Antoine Kahlouche <antoine@kahlouche.fr>
2026-04-15 10:45:37 +0200

    refactor(nvim): simplify plugin spec structure

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,19 +1,17 @@
 return {
-	{
-		"saghen/blink.cmp",
-		optional = true,
-		opts = {
-			keymap = {
-				preset = "none",
-				["<Tab>"] = { "select_next", "fallback" },
-				["<S-Tab>"] = { "select_prev", "fallback" },
-				["<CR>"] = { "accept", "fallback" },
-			},
-			completion = {
-				list = {
-					selection = {
-						preselect = false,
-					},
+	"saghen/blink.cmp",
+	optional = true,
+	opts = {
+		keymap = {
+			preset = "none",
+			["<Tab>"] = { "select_next", "fallback" },
+			["<S-Tab>"] = { "select_prev", "fallback" },
+			["<CR>"] = { "accept", "fallback" },
+		},
+		completion = {
+			list = {
+				selection = {
+					preselect = false,
 				},
 			},
 		},
